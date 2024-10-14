ff499f6ca2f50b4b5f07148c33225089d641ff7f
Matt Duncan <mmduncandev@gmail.com>
2024-10-14 06:16:52 -0400

    Initial Commit

---
+++ DollarDetective/DollarDetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarDetectiveApp.swift
+//  DollarDetective
+//
+//  Created by Matt Duncan on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DollarDetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
